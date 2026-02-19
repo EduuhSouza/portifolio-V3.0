18e28fe992cd5e843f94c30a0a41e52d7e67e7f8
EduuhSouza <146948069+EduuhSouza@users.noreply.github.com>
2026-02-19 14:37:05 -0300

    Background
    BAckground ajustado de acordo com o tamanho da tela e adicionado animação (img segue o mouse)

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ const intervalo = setInterval(() => {
   textoPorcentagem.textContent = progresso + '%';
 
 }, 150);
+
+// ANIMAÇÃO TELA
+
+const fundo = document.querySelector('.background');
+
+let mouseX = 0;
+let mouseY = 0;
+let posX = 0;
+let posY = 0;
+
+const forca = 30; // intensidade do movimento (20–40 ideal)
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX / window.innerWidth - 0.5;
+  const y = e.clientY / window.innerHeight - 0.5;
+
+  mouseX = x * forca;
+  mouseY = y * forca;
+});
+
+function atualizarFundo() {
+  // suavização (efeito câmera)
+  posX += (mouseX - posX) * 0.1;
+  posY += (mouseY - posY) * 0.1;
+
+  fundo.style.transform = `
+    translate(${posX}px, ${posY}px)
+    scale(1.06)
+  `;
+
+  requestAnimationFrame(atualizarFundo);
+}
+
+atualizarFundo();
+
+
